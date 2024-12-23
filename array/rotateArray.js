ae38425b16a743f20284ccbff8eb7c378770a55a
princegupta123 <princekumar39480@gmail.com>
2024-12-23 11:43:48 +0530

    solved rotateArray

--- array/rotateArray.js
+++ array/rotateArray.js
@@ -22,3 +22,24 @@ rotate 2 steps to the right: [3,99,-1,-100]
 */
 
 //solution
+
+function rotate(nums, k) {
+  k = k % nums.length;
+  //reverse entire array
+  reverse(nums, 0, nums.length - 1);
+  //reverse only k element
+  reverse(nums, 0, k - 1);
+  //reverse remaining elements
+  reverse(nums, k, nums.length - 1);
+  return nums;
+}
+function reverse(nums, start, end) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+}
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));

d352b724e28998e06e33b013c4271e46dc741fa6
princegupta123 <princekumar39480@gmail.com>
2025-01-24 13:01:52 +0530

    just removed console from longestCommonPrefix

--- array/longestCommonPrefix.js
+++ array/longestCommonPrefix.js
@@ -27,12 +27,9 @@ strs[i] consists of only lowercase English letters.
 function longestCommonPrefix(strs) {
   if (strs.length === 0) return "";
   let prefix = strs[0];
-  console.log(prefix, "prefix");
   for (let i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(prefix) !== 0) {
-      console.log("hello", strs[i].indexOf(prefix));
       prefix = prefix.substring(0, prefix.length - 1);
-      console.log("prefix 2", prefix);
       if (prefix === "") return "";
     }
   }

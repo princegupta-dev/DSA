8310018284e353ec0d3e13c1d99a20f2f4b73d3a
princegupta123 <princekumar39480@gmail.com>
2025-03-25 10:17:04 +0530

    solved group anagram

---
+++ hashmap/groupAnagram.js
@@ -0,0 +1,54 @@
+// 49. Group Anagrams
+
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+*/
+
+function groupAnagrams(strs) {
+  let map = new Map();
+  for (let str of strs) {
+    let sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) {
+      map.set(sortedStr, []);
+    }
+    map.get(sortedStr).push(str);
+  }
+  return Array.from(map.values());
+}
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams([""]));
+console.log(groupAnagrams(["a"]));

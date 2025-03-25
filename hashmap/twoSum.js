701d87009aa708385fc69694c773134f408722af
princegupta123 <princekumar39480@gmail.com>
2025-03-25 12:01:49 +0530

    push

--- hashmap/twoSum.js
+++ hashmap/twoSum.js
@@ -1,14 +1,10 @@
 // 1. Two Sum
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
- 
-
-Example 1:
+ Example 1:
 
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
@@ -43,3 +39,7 @@ function twoSum(nums, target) {
   }
   return [];
 }
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+console.log(twoSum([3, 2, 4], 6)); // [1,2]
+console.log(twoSum([3, 3], 6)); // [0,1]

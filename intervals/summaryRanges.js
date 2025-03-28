10ba9501b542be411fc760e37831b70b4754999a
princegupta123 <princekumar39480@gmail.com>
2025-03-28 15:23:38 +0530

    solved interval/summaryranges

---
+++ intervals/summaryRanges.js
@@ -0,0 +1,56 @@
+// 228. Summary Ranges
+/*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+ 
+
+Constraints:
+
+0 <= nums.length <= 20
+-231 <= nums[i] <= 231 - 1
+All the values of nums are unique.
+nums is sorted in ascending order.
+*/
+function summaryRanges(nums) {
+  let left = 0;
+  let arr = [];
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] + 1 !== nums[right + 1]) {
+      if (left === right) {
+        arr.push(`${nums[left]}`);
+      } else {
+        arr.push(`${nums[left]}->${nums[right]}`);
+      }
+      left = right + 1;
+    }
+  }
+  return arr;
+}
+console.log(summaryRanges([0, 1, 2, 4, 5, 7]));

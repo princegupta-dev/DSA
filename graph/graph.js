de029dce8d613d7f4d1c56997d4022f0aac1e2c4
Prince Gupta <prince.gupta@almond.solutions>
2025-10-07 19:38:31 +0530

    add: graph

---
+++ graph/graph.js
@@ -0,0 +1,33 @@
+class Graph {
+  constructor() {
+    this.adj = new Map();
+  }
+
+  // Add a vertex if not already in the graph
+  addVertex(v) {
+    if (!this.adj.has(v)) {
+      this.adj.set(v, []);
+    }
+  }
+  // Add an undirected edge (v <-> w)
+  addEdge(v, w) {
+    this.addVertex(v);
+    this.addVertex(w);
+    this.adj.get(v).push(w);
+    this.adj.get(w).push(v);
+  }
+
+  // Print the adjacency list
+  print() {
+    for (const [v, neighbors] of this.adj) {
+      console.log(v + " -> " + neighbors.join(", "));
+    }
+  }
+}
+
+const g = new Graph();
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+
+g.print();
